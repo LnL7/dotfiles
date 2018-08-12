b778a2e48b2ae5cda78afee1b756034f421d4759
Daiderd Jordan <daiderd@gmail.com>
2018-08-12 21:24:07 +0200

    nixpkgs: add impure chunkwm package

--- nixpkgs/overlays/50-darwin-impure-packages.nix
+++ nixpkgs/overlays/50-darwin-impure-packages.nix
@@ -2,6 +2,16 @@ self: super:
 
 {
   lnl = super.lnl or {} // {
+    # TODO: add chunkwm to nixpkgs. (CoreFoundation 10.11)
+    chunkwm = super.callPackage
+      ({ runCommandNoCC }:
+       runCommandNoCC "chunkwm-0.0.0" {} ''
+         mkdir -p $out/bin $out/libexec/chunkwm $out/lib/chunkwm/plugins
+         ln -s /src/chunkwm/src/chunkc/bin/chunkc $out/bin
+         ln -s /src/chunkwm/bin/chunkwm $out/libexec/chunkwm
+         ln -s /src/chunkwm/plugins/*.so $out/lib/chunkwm/plugins
+       '') {};
+
     # TODO: fix darwin build in nixpkgs.
     kitty = super.callPackage
       ({ runCommandNoCC }:
