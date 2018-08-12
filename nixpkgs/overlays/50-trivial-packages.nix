359e63122ffd9a4800b49033c7ebb5c8daf89a5c
Daiderd Jordan <daiderd@gmail.com>
2018-08-12 13:00:52 +0200

    nixpkgs: move simple expressions to trivial-packages.nix

--- nixpkgs/overlays/50-trivial-packages.nix
+++ nixpkgs/overlays/50-trivial-packages.nix
@@ -2,6 +2,26 @@ self: super:
 
 {
   lnl = super.lnl or {} // {
+    darwinpkgs = super.callPackage
+      ({ writeTextFile }:
+       writeTextFile {
+         name = "darwinpkgs";
+         destination = "/default.nix";
+         text = ''
+           { ... }@args: (import <darwin> args).pkgs
+         '';
+       }) {};
+
+    nixospkgs = super.callPackage
+      ({ writeTextFile }:
+       writeTextFile {
+         name = "nixospkgs";
+         destination = "/default.nix";
+         text = ''
+           { ... }@args: (import <nixos/nixos> args).pkgs
+         '';
+       }) {};
+
     git-statusbar = super.callPackage
       ({ stdenv, writeScriptBin, coreutils, gawk, git }:
        writeScriptBin "git-statusbar" ''
