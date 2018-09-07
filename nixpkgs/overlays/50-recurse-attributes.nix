b4f0b95a0a61785b5b014a39c100da0751526572
Daiderd Jordan <daiderd@gmail.com>
2018-09-07 22:13:59 +0200

    nixpkgs: recurse llvm6 package set

--- nixpkgs/overlays/50-recurse-attributes.nix
+++ nixpkgs/overlays/50-recurse-attributes.nix
@@ -1,6 +1,8 @@
 self: super:
 
 {
+  llvmPackages_6 = super.recurseIntoAttrs super.llvmPackages_6;
+
   darwin = super.recurseIntoAttrs (super.darwin // {
     apple_sdk = super.recurseIntoAttrs (super.darwin.apple_sdk // {
       frameworks = super.recurseIntoAttrs (super.darwin.apple_sdk.frameworks // {
