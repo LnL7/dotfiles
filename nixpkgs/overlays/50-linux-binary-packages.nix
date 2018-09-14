266ac47353d1c3cadb6ee201df10e9868a21d64a
Daiderd Jordan <daiderd@gmail.com>
2018-09-14 23:21:16 +0200

    nixpkgs: vault 0.10.4 -> 0.11.1

--- nixpkgs/overlays/50-linux-binary-packages.nix
+++ nixpkgs/overlays/50-linux-binary-packages.nix
@@ -5,10 +5,10 @@ self: super:
     vault = super.callPackage
       ({ stdenv, fetchurl, unzip }:
        stdenv.mkDerivation {
-         name = "vault-0.10.4";
+         name = "vault-0.11.1";
          src = fetchurl {
-           url = https://releases.hashicorp.com/vault/0.10.4/vault_0.10.4_linux_amd64.zip;
-           sha256 = "0fpi31mpnvy22g8a1383pf8h1ar0lsyxi8ywgnfjs6rgiwpadj5k";
+           url = https://releases.hashicorp.com/vault/0.11.1/vault_0.11.1_linux_amd64.zip;
+           sha256 = "15jrwqf58hr3x2xbkq7xrjpxx2wrjn3qfpq0j4grq96as1hj93gb";
          };
          strictDeps = true;
          nativeBuildInputs = [ unzip ];
