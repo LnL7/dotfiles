32ccb08ec1ac16ff343b28a8f973e6080a3f907e
Daiderd Jordan <daiderd@gmail.com>
2018-12-12 22:19:21 +0100

    nixpkgs: fix pow-dev on linux

--- nixpkgs/pkgs/puma-dev/default.nix
+++ nixpkgs/pkgs/puma-dev/default.nix
@@ -4,12 +4,19 @@ buildGoPackage rec {
   name = "puma-dev-${version}";
   version = "0.12";
   goPackagePath = "github.com/puma/puma-dev";
+
   src = fetchzip {
     url = "https://github.com/puma/puma-dev/archive/v${version}.tar.gz";
     sha256 = "01g1cwhdzff99k71d6r519j4fsjggcjb1iicc23ls3znfvibd7li";
   };
 
+  # darwin only package, breaks the linux build.
+  preBuild = stdenv.lib.optionalString stdenv.isLinux ''
+    rm -rv go/src/$goPackagePath/dev/launch
+  '';
+
   meta = with stdenv.lib; {
     maintainers = [ maintainers.lnl7 ];
+    platforms = platforms.darwin ++ platforms.linux;
   };
 }
