538ecee5641a81967473882309817b5529d1b84d
Daiderd Jordan <daiderd@gmail.com>
2018-12-21 18:33:53 +0100

    nixpkgs: pam_reattach init at 2018-09-27

--- nixpkgs/overlays/50-all-packages.nix
+++ nixpkgs/overlays/50-all-packages.nix
@@ -1,10 +1,18 @@
 self: super:
 
+let
+  inherit (super) callPackage darwin;
+in
+
 {
   lnl = super.lnl or {} // {
 
-    dev-tld-resolver = super.callPackage ../pkgs/dev-tld-resolver { };
-    puma-dev = super.callPackage ../pkgs/puma-dev { };
+    dev-tld-resolver = callPackage ../pkgs/dev-tld-resolver { };
+    puma-dev = callPackage ../pkgs/puma-dev { };
+
+    pam_reattach = callPackage ../pkgs/pam_reattach {
+      inherit (darwin.apple_sdk.libs) xpc;
+    };
 
   };
 }
---
+++ nixpkgs/pkgs/pam_reattach/default.nix
@@ -0,0 +1,17 @@
+{ stdenv, fetchzip, cmake, pam, xpc }:
+
+stdenv.mkDerivation {
+  name = "pam_reattach-2018-09-27";
+
+  src = fetchzip {
+    url = "https://github.com/fabianishere/pam_reattach/archive/3326a4b085f19becaaed494b8d711cda78c11bbf.tar.gz";
+    sha256 = "1k16gxxnxa7r6c0gw6rcx5x4g8hmm70pavvg6x9ijkv6s0527h2g";
+  };
+
+  nativeBuildInputs = [ cmake ];
+  buildInputs = [ pam xpc ];
+
+  meta = with stdenv.lib; {
+    platforms = platforms.darwin;
+  };
+}
