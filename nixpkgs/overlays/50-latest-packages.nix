3bcfddf5f9e911bcb59aea2f32746c24f84019ae
Daiderd Jordan <daiderd@gmail.com>
2018-12-23 12:32:41 +0100

    nixpkgs: remove latest-packages.nix

--- nixpkgs/overlays/50-latest-packages.nix
+++
@@ -1,9 +0,0 @@
-self: super:
-
-{
-  latest = super.latest or {} // {
-    cachix = import (builtins.fetchTarball https://github.com/cachix/cachix/archive/master.tar.gz) {};
-    nix = import "${builtins.fetchTarball https://github.com/NixOS/nix/archive/master.tar.gz}/release.nix" {};
-    nixpkgs = import (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/master.tar.gz) {};
-  };
-}
