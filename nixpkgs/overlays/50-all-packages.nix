5469d3a2dfb80a085220c65c67b35f318bf2ff84
Daiderd Jordan <daiderd@gmail.com>
2018-12-23 12:21:10 +0100

    nixpkgs: fix puma-dev

--- nixpkgs/overlays/50-all-packages.nix
+++ nixpkgs/overlays/50-all-packages.nix
@@ -8,7 +8,9 @@ in
   lnl = super.lnl or {} // {
 
     dev-tld-resolver = callPackage ../pkgs/dev-tld-resolver { };
-    puma-dev = callPackage ../pkgs/puma-dev { };
+    puma-dev = callPackage ../pkgs/puma-dev {
+      buildGoPackage = super.buildGo110Package;
+    };
 
     pam_reattach = callPackage ../pkgs/pam_reattach {
       inherit (darwin.apple_sdk.libs) xpc;
