d9b670138576c934e8adbd0f23e23afb3ccd2ab2
Daiderd Jordan <daiderd@gmail.com>
2019-09-29 23:28:16 +0200

    vim: fixup plugin loading
    For plugins with eg. syntax :packadd won't have priority over the
    builtin definitions for some reason.

--- nixpkgs/overlays/50-vim-packages.nix
+++ nixpkgs/overlays/50-vim-packages.nix
@@ -1,13 +1,27 @@
 self: super:
 
 let
+  loadPlugin = p: ''
+    set rtp^=${p.rtp}
+    set rtp+=${p.rtp}/after
+  '';
+
+  plugins = ''
+    " Workaround for broken handling of packpath by vim8/neovim for ftplugins
+    filetype off | syn off
+    ${super.lib.concatStrings (map loadPlugin startPackages)}
+    filetype indent plugin on | syn on
+  '';
+
   startPackages = with super.vimPlugins; [
     sensible repeat surround
     ReplaceWithRegister vim-indent-object vim-sort-motion
     fzfWrapper fzf-vim vim-dispatch vim-test vim-projectionist vim-gitgutter
     vim-abolish commentary vim-eunuch fugitive vim-isort rhubarb vim-scriptease tabular vim-tbone
     gist-vim webapi-vim
-    polyglot vim-nix bats-vim vim-docbk editorconfig-vim
+    polyglot vim-nix bats-vim vim-docbk editorconfig-vim hoogle
+
+    # direnv-vim
   ];
 in
 
@@ -16,16 +30,17 @@ in
     neovim = super.neovim.override {
       configure = {
         packages.darwin.start = startPackages
-          ++ (with super.vimPlugins; [ deoplete-nvim ale ]);
+          ++ (with super.vimPlugins; [ ale deoplete-nvim ]);
         packages.darwin.opt = with super.vimPlugins; [
           colors-solarized
-          echodoc-vim
           splice-vim
 
-          LanguageClient-neovim
-          neomake
+          # LanguageClient-neovim coc-nvim neomake
+          echodoc-vim
         ];
         customRC = ''
+          ${plugins}
+
           source ${../../vim/vimrc}
 
           set clipboard=unnamed
@@ -35,13 +50,14 @@ in
           set relativenumber
 
           " ale
+          let g:ale_elixir_elixir_ls_release = '${self.lnl.elixir-ls}/share/elixir-ls'
           set omnifunc=ale#completion#OmniFunc
           nnoremap <Leader>d :ALEGoToDefinition<CR>
           nnoremap <Leader>D :ALEGoToDefinitionInVSplit<CR>
+          nnoremap <Leader>k :ALESignature<CR>
           nnoremap <Leader>K :ALEHover<CR>
           nnoremap [a :ALEPreviousWrap<CR>
           nnoremap ]a :ALENextWrap<CR>
-          let g:ale_elixir_elixir_ls_release = '${self.lnl.elixir-ls}/share/elixir-ls'
 
           " let g:ale_completion_enabled = 1
 
@@ -50,6 +66,12 @@ in
           inoremap <expr><C-l> deoplete#refresh()
           inoremap <silent><expr><C-Tab> deoplete#mappings#manual_complete()
           inoremap <silent><expr><Tab> pumvisible() ? "\<C-n>" : "\<TAB>"
+
+          let g:deoplete#enable_at_startup = 1
+
+          " echodoc
+          " let g:echodoc_enable_at_startup = 1
+          " set noshowmode
 
           if filereadable($HOME . '/.vimrc')
             source ~/.vimrc
@@ -61,23 +83,15 @@ in
     vim = super.vim_configurable.customize {
       name = "vim";
       vimrcConfig.packages.darwin.start = startPackages
-        ++ (with super.vimPlugins; [ YouCompleteMe ale ]);
+        ++ (with super.vimPlugins; [ ale ]);
       vimrcConfig.packages.darwin.opt = with super.vimPlugins; [
         colors-solarized
-        echodoc-vim
         splice-vim
       ];
       vimrcConfig.customRC = ''
+        ${plugins}
+
         source ${../../vim/vimrc}
-
-        " youcompleteme
-        nmap <Leader>d :YcmCompleter GoTo<CR>
-        nmap <Leader>K :YcmCompleter GetDoc<CR>
-
-        nmap <Leader>jt :YcmCompleter GetType<CR>
-        nmap <Leader>jc :YcmCompleter GoToDeclaration<CR>
-        nmap <Leader>jf :YcmCompleter GoToDefinition<CR>
-        nmap <Leader>jr :YcmCompleter GoToReferences<CR>
 
         if filereadable($HOME . '/.vimrc')
           source ~/.vimrc
