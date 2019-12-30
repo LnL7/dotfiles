9fdd8c3a3484e5450dc664397ad0266f37a7293a
Daiderd Jordan <daiderd@gmail.com>
2019-12-30 10:02:15 +0100

    nixpkgs: puma 3.12.0 -> 4.3.1

--- nixpkgs/pkgs/puma/gemset.nix
+++ nixpkgs/pkgs/puma/gemset.nix
@@ -1,22 +1,33 @@
 {
-  puma = {
+  nio4r = {
     groups = ["default"];
     platforms = [];
     source = {
       remotes = ["https://rubygems.org"];
-      sha256 = "1k7dqxnq0dnf5rxkgs9rknclkn3ah7lsdrk6nrqxla8qzy31wliq";
+      sha256 = "0gnmvbryr521r135yz5bv8354m7xn6miiapfgpg1bnwsvxz8xj6c";
       type = "gem";
     };
-    version = "3.12.0";
+    version = "2.5.2";
+  };
+  puma = {
+    dependencies = ["nio4r"];
+    groups = ["default"];
+    platforms = [];
+    source = {
+      remotes = ["https://rubygems.org"];
+      sha256 = "0v6zai6sinw5r1lchm278mm3dr8x5vi8pwmybwv9lz1kz02fk2g3";
+      type = "gem";
+    };
+    version = "4.3.1";
   };
   rack = {
     groups = ["default"];
     platforms = [];
     source = {
       remotes = ["https://rubygems.org"];
-      sha256 = "1pcgv8dv4vkaczzlix8q3j68capwhk420cddzijwqgi2qb4lm1zm";
+      sha256 = "1id0jsslx1ipv0pbqjfn7mjbb2vx2xybk7qypq59a17163xp30gr";
       type = "gem";
     };
-    version = "2.0.6";
+    version = "2.0.8";
   };
 }
