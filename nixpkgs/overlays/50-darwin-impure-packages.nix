1ed103cff068bec42b7698543d94167ba8b06fa7
Daiderd Jordan <daiderd@gmail.com>
2020-02-24 22:42:22 +0100

    nixpkgs: add impure mpv swift build

--- nixpkgs/overlays/50-darwin-impure-packages.nix
+++ nixpkgs/overlays/50-darwin-impure-packages.nix
@@ -1,21 +1,45 @@
 self: super:
 
+let
+  CommandLineTools = "/Library/Developer/CommandLineTools";
+in
+
 {
   lnl = super.lnl or {} // {
-    CommandLineTools = super.callPackage
-      ({ makeSetupHook }:
-       makeSetupHook {} (builtins.toFile "CommandLineTools" ''
-         addCommandLineTools() {
-             echo >&2
-             echo "WARNING: this is impure and unreliable, make sure the CommandLineTools are installed!" >&2
-             echo "  $ xcode-select --install" >&2
-             echo >&2
-             [ -d /Library/Developer/CommandLineTools/usr/bin ]
-             export PATH=/usr/bin:/Library/Developer/CommandLineTools/usr/bin:$PATH
-         }
+    swift = super.stdenv.mkDerivation {
+      name = "swift-CommandLineTools-0.0.0";
+      phases = [ "installPhase" "fixupPhase" ];
 
-         prePhases+=" addCommandLineTools"
-       '')) {};
+      propagatedBuildInputs = [ self.darwin.DarwinTools ];
+
+      installPhase = ''
+          mkdir -p $out/bin $out/lib
+          ln -s ${CommandLineTools}/usr/bin/swift $out/bin
+          ln -s ${CommandLineTools}/usr/lib/swift $out/lib
+          ln -s ${CommandLineTools}/SDKs $out
+      '';
+
+      setupHook = builtins.toFile "hook" ''
+          addCommandLineTools() {
+              echo >&2
+              echo "WARNING: this is impure and unreliable, make sure the CommandLineTools are installed!" >&2
+              echo "  $ xcode-select --install" >&2
+              echo >&2
+              [ -d ${CommandLineTools} ]
+              export NIX_LDFLAGS+=" -L@out@/lib/swift/macosx"
+              export SWIFT=swift
+              export SWIFT_LIB_DYNAMIC=@out@/lib/swift/macosx
+              export MACOS_SDK_VERSION=$(sw_vers -productVersion | awk -F. '{print $1 "." $2}')
+              export MACOS_SDK=@out@/SDKs/MacOSX$MACOS_SDK_VERSION.sdk
+          }
+
+          prePhases+=" addCommandLineTools"
+      '';
+
+      __impureHostDeps = [ CommandLineTools ];
+    };
+
+    mpv = super.mpv.override { swiftSupport = true; inherit (self.lnl) swift; };
 
     # TODO: fix darwin build in nixpkgs.
     kitty = super.callPackage
