4799c7b1b588f81c9d104c5ad5f17ff9c2906892
Daiderd Jordan <daiderd@gmail.com>
2020-02-26 21:19:21 +0100

    vim: add pytest compiler
    Makes running tests with dispatch much nicer, especially since pytest
    prints banners based on the terminal width which is bigger than the
    quickfix window.

--- nixpkgs/overlays/50-vim-packages.nix
+++ nixpkgs/overlays/50-vim-packages.nix
@@ -13,19 +13,32 @@ let
     filetype indent plugin on | syn on
   '';
 
-  startPackages = with super.vimPlugins; [
+  startPackages = with self.vimPlugins; [
     sensible repeat surround
     ReplaceWithRegister vim-indent-object vim-sort-motion
     fzfWrapper fzf-vim vim-dispatch vim-test vim-projectionist vim-gitgutter
     vim-abolish commentary vim-eunuch fugitive rhubarb vim-scriptease tabular vim-tbone
     gist-vim webapi-vim
-    polyglot vim-nix bats-vim vim-docbk editorconfig-vim hoogle
+    polyglot vim-nix bats-vim pytest-vim-compiler editorconfig-vim
 
     # direnv-vim
   ];
 in
 
 {
+  vimPlugins = super.vimPlugins // {
+    pytest-vim-compiler = super.vimUtils.buildVimPluginFrom2Nix {
+      pname = "pytest-vim-compiler";
+      version = "2015-05-28";
+      src = super.fetchFromGitHub {
+        owner = "5long";
+        repo = "pytest-vim-compiler";
+        rev = "f7c4cd0302c7baedffb45857e63eb4182d8ac45b";
+        sha256 = "1j4nn26yfl17aid85qahc7li95wmkp8lg2z4q1jq2q16lda14ws8";
+      };
+    };
+  };
+
   lnl = super.lnl or {} // {
     neovim = super.neovim.override {
       configure = {
