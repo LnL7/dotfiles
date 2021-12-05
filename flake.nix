f958545dea85c591eced1e43d06a40e3a2acf374
Daiderd Jordan <daiderd@gmail.com>
2021-12-05 16:21:54 +0100

    nixpkgs: add overlay set for nixos

--- flake.nix
+++ flake.nix
@@ -82,6 +82,21 @@
 
       };
 
+    nixosOverlays = [
+      (self: super: {
+        lnl = super.lnl or {} // {
+          inherit vim-nix;
+        };
+      })
+
+      (import ./nixpkgs/overlays/10-dev-overrides.nix)
+      (import ./nixpkgs/overlays/20-trivial-overrides.nix)
+      (import ./nixpkgs/overlays/50-all-packages.nix)
+      (import ./nixpkgs/overlays/50-linux-binary-packages.nix)
+      (import ./nixpkgs/overlays/50-trivial-packages.nix)
+      (import ./nixpkgs/overlays/50-vim-packages.nix)
+    ];
+
     darwinOverlays = [
       (self: super: {
         lnl = super.lnl or {} // {
@@ -94,7 +109,6 @@
       (import ./nixpkgs/overlays/20-trivial-overrides.nix)
       (import ./nixpkgs/overlays/50-all-packages.nix)
       (import ./nixpkgs/overlays/50-darwin-impure-packages.nix)
-      (import ./nixpkgs/overlays/50-nixpkgs-channels.nix)
       (import ./nixpkgs/overlays/50-recurse-attributes.nix)
       (import ./nixpkgs/overlays/50-trivial-packages.nix)
       (import ./nixpkgs/overlays/50-vim-packages.nix)
